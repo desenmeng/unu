f2c81202be03bd5bb4898dc3f9a2221732727b10
mdemo <mengdesen09@qq.com>
2014-07-29 18:35:28 +0800

    add error for callback

--- example/index.js
+++ example/index.js
@@ -2,7 +2,8 @@
  * Created by mdemo on 2014/7/29.
  */
 var unu = require('..');
-unu('http://m.baidu.com',{userAgent:'IPHONE'},function(result){
-    console.log(result.unused);
-    console.log(result.used);
+unu('http://m.baidu.com/s?word=love',function(error,result){
+    if(!error){
+        console.log(result);
+    }
 });
