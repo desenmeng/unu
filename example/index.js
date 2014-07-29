1d85706daf39babfd8f93dd21439bf18a7e77120
mdemo <mengdesen09@qq.com>
2014-07-29 14:13:31 +0800

    v0.0.2

--- example/index.js
+++ example/index.js
@@ -1,13 +1,8 @@
-unu
-===
-
-unused css
-
-```
-var unu = require('unu');
+/**
+ * Created by mdemo on 2014/7/29.
+ */
+var unu = require('..');
 unu('http://m.baidu.com',function(result){
     console.log(result.unused);
     console.log(result.used);
 });
-
-```
