f9d0773d2ca2a965f4178351f356efd5b9e031a7
mdemo <mengdesen09@qq.com>
2014-07-30 10:30:06 +0800

    update for url with chinese

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@
  * Created by mdemo on 2014/7/29.
  */
 var unu = require('..');
-unu('http://m.baidu.com/s?word=love',function(error,result){
+unu('http://m.baidu.com/s?word=北京天气',function(error,result){
     if(!error){
         console.log(result);
     }
